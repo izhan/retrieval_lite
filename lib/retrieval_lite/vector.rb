945dcf21ea547736b8a74ecac6217fa2295a25db
Irvin Zhan <izhan@princeton.edu>
2014-05-11 22:37:24 -0400

    convert vector from hash to array

--- lib/retrieval_lite/vector.rb
+++ lib/retrieval_lite/vector.rb
@@ -1,41 +1,34 @@
 module RetrievalLite::Vector
-  # @param scores1 [Hash<String, Integer>] each term and its corresponding score in the first document
-  # @param scores2 [Hash<String, Integer>] each term and its corresponding score in the second document
+  # @param scores1 [Array<Integer>] each term and its corresponding score in the first document
+  # @param scores2 [Array<Integer>] each term and its corresponding score in the second document
   # @return [Float] the cosine similarity of the two vectors representing the score of the documents
   def self.cosine_similarity(scores1, scores2)
-    dot_product(frequencies1, frequencies2) / (euclidean_length(frequencies1) * euclidean_length(frequencies2))
+    dot_product(scores1, scores2) / (euclidean_length(scores1) * euclidean_length(scores2))
   end
 
-  # @param scores1 [Hash<String, Integer>] each term and its corresponding score in the first document
-  # @param scores2 [Hash<String, Integer>] each term and its corresponding score in the second document
+  # @param scores1 [Array<Integer>] each term and its corresponding score in the first document
+  # @param scores2 [Array<Integer>] each term and its corresponding score in the second document
   # @return [Float] the dot product of the two vectors representing the score of the documents
   def self.dot_product(scores1, scores2)
-    sum = 0
-    if scores1.size > scores2.size
-      scores1.each do |term, count|
-        if scores2[term] != nil
-          sum += count * scores2[term]
-        end
-      end
-    else
-      scores2.each do |term, count|
-        if scores1[term] != nil
-          sum += count * scores1[term]
-        end
-      end
-    end
+    raise "document vectors are not of same length" if scores1.size != scores2.size
     
+    sum = 0
+    for i in 0...scores1.size
+      sum += scores1[i]*scores2[i]
+    end
+
     return sum
   end
 
-  # @param scores [Hash<String, Integer>] each term and its corresponding score in the document
+  # @param scores [Array<Integer>] each term and its corresponding score in the document
   # @return [Float] the euclidean length of the vectors representing the score of the document
   def self.euclidean_length(scores)
     sum = 0
 
-    scores.each do |term, count|
-      sum += count * count
+    for i in 0...scores.size
+      sum += scores[i] * scores[i]
     end
+
     Math.sqrt(sum)
   end
 end
