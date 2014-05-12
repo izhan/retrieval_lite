6e91982cd6052a6e36abdad43968d4d916158fb0
Irvin Zhan <izhan@princeton.edu>
2014-05-11 22:33:03 -0400

    finish vector model

--- lib/retrieval_lite/vector.rb
+++ lib/retrieval_lite/vector.rb
@@ -10,12 +10,32 @@ module RetrievalLite::Vector
   # @param scores2 [Hash<String, Integer>] each term and its corresponding score in the second document
   # @return [Float] the dot product of the two vectors representing the score of the documents
   def self.dot_product(scores1, scores2)
+    sum = 0
+    if scores1.size > scores2.size
+      scores1.each do |term, count|
+        if scores2[term] != nil
+          sum += count * scores2[term]
+        end
+      end
+    else
+      scores2.each do |term, count|
+        if scores1[term] != nil
+          sum += count * scores1[term]
+        end
+      end
+    end
     
+    return sum
   end
 
   # @param scores [Hash<String, Integer>] each term and its corresponding score in the document
   # @return [Float] the euclidean length of the vectors representing the score of the document
   def self.euclidean_length(scores)
-    
+    sum = 0
+
+    scores.each do |term, count|
+      sum += count * count
+    end
+    Math.sqrt(sum)
   end
 end
